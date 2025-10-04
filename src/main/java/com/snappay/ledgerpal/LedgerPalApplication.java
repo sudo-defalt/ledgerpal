3a52c5962c0a85f06f39f1230226960c90eb6bc2
adnan <a.aliyari@gmail.com>
2025-10-04 23:43:03 +0330

    init

---
+++ src/main/java/com/snappay/ledgerpal/LedgerPalApplication.java
@@ -0,0 +1,13 @@
+package com.snappay.ledgerpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgerPalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LedgerPalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snappay/ledgerpal/LedgerPalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snappay.ledgerpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerPalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
