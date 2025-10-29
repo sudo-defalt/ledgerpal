cff7dd254166a144c239bafef02d4edba623f844
adnan <a.aliyari@gmail.com>
2025-10-30 00:02:37 +0330

    fix(migration) fix sequence naming issue in V1__init.sql migration file

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,44 +1,49 @@
+create sequence ledger.users_seq;
 create table ledger.users (
-   id bigserial not null unique primary key,
+   id bigint not null unique primary key default nextval('ledger.users_seq'),
    uuid uuid not null unique,
    username varchar(255) not null unique,
    email varchar(255) not null unique,
    password varchar(255) not null
 );
 
+create sequence ledger.authorities_seq;
 create table ledger.authorities (
-   id bigserial not null unique primary key,
+   id bigint not null unique primary key default nextval('ledger.authorities_seq'),
    name varchar(255) not null unique,
    description varchar(255)
 );
 
 create table ledger.user_authorities (
-   user_id bigserial not null references ledger.users,
-   authority_id bigserial not null references ledger.authorities,
+   user_id bigint not null references ledger.users,
+   authority_id bigint not null references ledger.authorities,
    primary key (user_id, authority_id)
 );
 
+create sequence ledger.accounts_seq;
 create table ledger.accounts (
-   id bigserial not null unique primary key,
+   id bigint not null unique primary key default nextval('ledger.accounts_seq'),
    uuid uuid not null unique,
    name varchar(255),
    balance bigint not null,
-   user_id bigserial not null references ledger.users
+   user_id bigint not null references ledger.users
 );
 
+create sequence ledger.categories_seq;
 create table ledger.categories (
-   id bigserial not null unique primary key,
+   id bigint not null unique primary key default nextval('ledger.categories_seq'),
    uuid UUID not null unique,
    name varchar(255) not null,
-   user_id bigserial not null references ledger.users
+   user_id bigint not null references ledger.users
 );
 
+create sequence ledger.transactions_seq;
 CREATE TABLE ledger.transactions (
-   id bigserial not null unique primary key,
+   id bigint not null unique primary key default nextval('ledger.transactions_seq'),
    uuid uuid not null unique,
    created_at timestamp without time zone not null,
    amount bigint not null,
    description varchar(511),
-   account_id bigserial not null references ledger.accounts,
-   category_id bigserial references ledger.categories
+   account_id bigint not null references ledger.accounts,
+   category_id bigint references ledger.categories
 );
